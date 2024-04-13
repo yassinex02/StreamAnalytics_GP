ee0a1a6a3bb5774c6b93aab8e6d5e21bf5085e13
zaidmehdi <mehdizaid02@gmail.com>
2024-04-13 12:39:30 +0200

    intial logic

---
+++ src/personality.py
@@ -0,0 +1,39 @@
+
+
+
+
+
+def get_familiarity_exploration_score(user_id):
+    return
+
+
+def get_loyalty_variety_score(user_id):
+    return
+
+
+def get_timelessness_newness_score(user_id):
+    return
+
+
+def get_commonality_uniqueness_score(user_id):
+    return
+
+
+def get_personality_traits(user_id):
+    fe_score = get_familiarity_exploration_score(user_id)
+
+
+def get_personality_type(user_id):
+    personality_map = {
+
+    }
+
+    return 
+
+def main():
+    return
+
+
+
+if __name__ == "__main__":
+    main()
