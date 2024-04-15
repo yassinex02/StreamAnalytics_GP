15f80304a8220baf1098f5f65ccc77f0b3933d9d
zaidmehdi <mehdizaid02@gmail.com>
2024-04-16 01:58:02 +0200

    style index page

--- src/app/app.py
+++ src/app/app.py
@@ -8,8 +8,15 @@ def index():
   if request.method == 'POST':
     user_input = request.form['user_input']
     return redirect(url_for('wrapped', user_id=user_input))
+  
+  context = {
+      "top_songs_week": ["Song1", "Song2", "Song3", "Song4", "Song5"],
+      "top_songs_month": ["So1", "Song2", "Song3", "Song4", "Song5"],
+      "top_artists_week": ["Artist1", "Artist2", "Artist3", "Artist4", "Artist5"],
+      "top_artists_month": ["Artist1", "Artist2", "Artist3", "Artist4", "Artist5"],
+  }
 
-  return render_template('index.html')
+  return render_template('index.html', **context)
 
 
 @app.route('/wrapped/<user_id>')
@@ -17,10 +24,7 @@ def wrapped(user_id):
     context = {
        "user_id": user_id,
        "personality_type": "The Voyager",
-       "first_trait": "Exploration",
-       "second_trait": "Newness",
-       "third_trait": "Loyalty",
-       "fourth_trait": "Commonality",
+       "personality_traits": ["Exploration", "Newness", "Loyalty", "Commonality"]
     }    
 
     return render_template('wrapped.html', **context)
